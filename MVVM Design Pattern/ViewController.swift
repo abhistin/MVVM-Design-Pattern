19387a5d6d05f98f849d1bc4910a73259c769daf
Abhishek Bhardwaj <abhishek.bhardwaj@edriving.com>
2024-04-17 12:11:37 +0530

    Initial Commit

---
+++ MVVM Design Pattern/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVM Design Pattern
+//
+//  Created by Abhishek Bhardwaj on 17/04/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
